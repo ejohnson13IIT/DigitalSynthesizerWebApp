db4fc2bdcefe93226106880c2bda09d790cc275c
Ejohnson13IIT <ejohnson13@hawk.iit.edu>
2025-10-30 21:49:51 -0500

    Removing some comments

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO
 import socket
 from pythonosc.udp_client import SimpleUDPClient
 
 app = Flask(__name__)
+
+
 socketio = SocketIO(app)
 osc_ip = "127.0.0.1"   # change if your OSC target is on another device
 osc_port = 5005        # Carlas OSC UDP port (make sure this matches Carla)
@@ -19,9 +21,7 @@ def handle_knob_change(data):
     value = data["value"]
     rackID=data["rack"]
     sentMsg="/Carla/"+str(rackID)+"/set_parameter_value"
-    print(f"Knob Change - Rack: {rackID}, Parameter: {parameterID}, Value: {value}")
     client.send_message(sentMsg, [parameterID, (value/100)*48-24])
-    
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
