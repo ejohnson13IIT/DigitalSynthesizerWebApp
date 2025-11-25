0a1860037155de30356e7b135fc614605f5f00a6
mtjainam <mtjainam23@gmail.com>
2025-11-25 02:20:52 -0600

    Update pot_test.py

--- potdemo/pot_test.py
+++ potdemo/pot_test.py
@@ -4,14 +4,12 @@ from gpiozero import PWMLED, MCP3008
 from time import sleep
 from pythonosc.udp_client import SimpleUDPClient
 
-# Physical knobs left to right: pot1, pot2, pot3, pot4
-# Remapped: old pot4 -> new pot1, old pot1 -> new pot2, old pot3 stays, old pot2 -> new pot4
-pot1 = MCP3008(3)  # Rightmost knob (was pot4) -> parameter 1
-pot2 = MCP3008(0)  # Leftmost knob (was pot1) -> parameter 3
-pot3 = MCP3008(2)  # 2nd from right (keep as is) -> parameter 2 (mix)
-pot4 = MCP3008(1)  # 2nd from left (was pot2) -> needs fixing
-osc_ip = "127.0.0.1"   # change if your OSC target is on another device
-osc_port = 28017        # Carlas OSC UDP port (make sure this matches Carla)
+pot1 = MCP3008(0)
+pot2 = MCP3008(1)
+pot3 = MCP3008(2)
+pot4 = MCP3008(3)
+osc_ip = "127.0.0.1"
+osc_port = 28017
 client = SimpleUDPClient(osc_ip, osc_port)
 
 lastVal1=0
@@ -36,22 +34,22 @@ def map_pot_to_mix(pot_value):
     return pot_value
 
 while True:
-	
 	if abs(int(pot1.value * 100) - lastVal1) > 2:
-		semitone_value = map_pot_to_semitone(pot1.value)
-		client.send_message("/Carla/0/set_parameter_value", [1, semitone_value])
+		octave_value = map_pot_to_octave(pot1.value)
+		client.send_message("/Carla/0/set_parameter_value", [0, octave_value])
 	
 	if abs(int(pot2.value * 100) - lastVal2) > 2:
-		octave_value = map_pot_to_octave(pot2.value)
-		client.send_message("/Carla/0/set_parameter_value", [3, octave_value])
+		semitone_value = map_pot_to_semitone(pot2.value)
+		client.send_message("/Carla/0/set_parameter_value", [1, semitone_value])
 	
 	if abs(int(pot3.value * 100) - lastVal3) > 2:
 		mix_value = map_pot_to_mix(pot3.value)
 		client.send_message("/Carla/0/set_parameter_value", [2, mix_value])
 	
 	if abs(int(pot4.value * 100) - lastVal4) > 2:
-		semitone_value = map_pot_to_semitone(pot4.value)
-		client.send_message("/Carla/0/set_parameter_value", [0, semitone_value])
+		octave_value = map_pot_to_octave(pot4.value)
+		client.send_message("/Carla/0/set_parameter_value", [3, octave_value])
+	
 	lastVal1 = int(pot1.value * 100)
 	lastVal2 = int(pot2.value * 100)
 	lastVal3 = int(pot3.value * 100)
